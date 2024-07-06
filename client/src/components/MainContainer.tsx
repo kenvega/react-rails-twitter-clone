a4948fe8b18536b4b6571aee3edacf3d9dcb66d2
Ken Vega <ken.vega.g@gmail.com>
2024-07-06 18:42:07 -0500

    main - add prettierrc

--- client/src/components/MainContainer.tsx
+++ client/src/components/MainContainer.tsx
@@ -11,22 +11,29 @@ import Message from "../assets/email.svg";
 import Bookmark from "../assets/bookmark-unfilled.svg";
 import Profile from "../assets/profile.svg";
 
+import { useNavigate } from "react-router-dom";
 
 const MainContainer = () => {
+  const navigate = useNavigate();
+
+  const goTo = (route: string) => {
+    navigate(route);
+  };
+
   return (
     <div className="flex gap-4">
       <div>
         <div className="px-6 py-2 flex align-middle gap-5 mb-4 rounded-full">
           <img src={Twitter} alt="Your SVG" width={32} className="" />
         </div>
-        <SidebarButton title="Home" SVGIcon={Home} active />
-        <SidebarButton title="Hashtags" SVGIcon={Hashtag} />
-        <SidebarButton title="All Tweets" SVGIcon={Earth} />
-        <SidebarButton title="All Users" SVGIcon={Users} />
-        <SidebarButton title="Notifications" SVGIcon={Notification} />
-        <SidebarButton title="Messages" SVGIcon={Message} />
-        <SidebarButton title="Bookmarks" SVGIcon={Bookmark} />
-        <SidebarButton title="Profile" SVGIcon={Profile} />
+        <SidebarButton title="Home" SVGIcon={Home} goTo={goTo("/home")} active />
+        <SidebarButton title="Hashtags" SVGIcon={Hashtag} goTo={goTo("/home")} />
+        <SidebarButton title="All Tweets" SVGIcon={Earth} goTo={goTo("/home")} />
+        <SidebarButton title="All Users" SVGIcon={Users} goTo={goTo("/home")} />
+        <SidebarButton title="Notifications" SVGIcon={Notification} goTo={goTo("/home")} />
+        <SidebarButton title="Messages" SVGIcon={Message} goTo={goTo("/home")} />
+        <SidebarButton title="Bookmarks" SVGIcon={Bookmark} goTo={goTo("/home")} />
+        <SidebarButton title="Profile" SVGIcon={Profile} goTo={goTo("/home")} />
       </div>
       <div>
         <TweetsList />
--- client/src/components/SidebarButton.tsx
+++ client/src/components/SidebarButton.tsx
@@ -2,16 +2,19 @@ const SidebarButton = ({
   active,
   SVGIcon,
   title,
+  goTo
 }: {
   active?: boolean;
   SVGIcon: string;
   title: string;
+  goTo: () => {}
 }) => {
   return (
     <div
       className={`rounded-full px-6 py-2 flex align-middle gap-5 mb-4 ${
         active ? "bg-neutral-200" : ""
       }`}
+      onClick={goTo}
     >
       <div>
         <img src={SVGIcon} alt="Your SVG" width={32} />
