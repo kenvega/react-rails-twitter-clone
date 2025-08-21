d49d8d6e2361b136fddba4656c031be19c8dab97
Ken Vega <ken.vega.g@gmail.com>
2025-08-21 11:44:41 -0500

    main - wip adding tweet reply form

--- client/src/components/TweetPage.tsx
+++ client/src/components/TweetPage.tsx
@@ -1,8 +1,22 @@
 import Sidebar from "./Sidebar";
 import TweetContainer from "./TweetContainer";
 import HashtagSidebar from "./HashtagSidebar";
+import TweetReplyForm from "./TweetReplyForm";
+
+import { useState } from "react";
+
+import { createReplyTweet, fetchReplyTweets } from "../services/tweetsService";
 
 const TweetPage = () => {
+  const [tweetBody, setTweetBody] = useState("");
+
+  const handleFormSubmit = () => {
+    createReplyTweet({ tweetBody }).then(() => {
+      fetchReplyTweets(); // tendrias que enviar el id, o quizas hacer un fetch de nuevo a todo el tweet con replies?
+      setTweetBody("");
+    });
+  };
+
   return (
     <div className="py-4 min-h-screen dark:bg-gray-800">
       <div className="flex flex-row max-w-7xl mx-auto">
@@ -12,6 +26,9 @@ const TweetPage = () => {
 
         <div className="basis-3/5">
           <TweetContainer />
+          <TweetReplyForm tweetBody={tweetBody} setTweetBody={setTweetBody} onSubmit={handleFormSubmit} />
+
+          {/* <TweetReplyForm /> */}
           {/* TweetReplies (?) */}
         </div>
 
---
+++ client/src/components/TweetReplyForm.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, FormEvent, MouseEvent } from "react";
+import { Button } from "@radix-ui/themes";
+
+const TweetReplyForm = ({
+  tweetBody,
+  setTweetBody,
+  onSubmit,
+}: {
+  tweetBody: string;
+  setTweetBody: (value: string) => void;
+  onSubmit: () => void;
+}) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTweetBody(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement> | MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <div className="p-4 rounded-md hover:bg-slate-200 dark:hover:bg-cyan-900/40">
+      <form onSubmit={handleSubmit}>
+        {/* TODO: when submitting it should block the button (?) */}
+        <input
+          className="w-full rounded-sm px-2 py-2 mb-3 dark:text-stone-600"
+          type="text"
+          placeholder="What's happening?"
+          value={tweetBody}
+          onChange={handleChange}
+        />
+
+        <div className="flex justify-end">
+          <Button type="submit">Tweet</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TweetReplyForm;
