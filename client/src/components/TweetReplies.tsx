bf1dc945b81fe355f854205983700b5e9132abdf
Ken Vega <ken.vega.g@gmail.com>
2025-08-28 12:37:35 -0500

    main - style a bit tweet replies

--- client/src/components/TweetReplies.tsx
+++ client/src/components/TweetReplies.tsx
@@ -7,9 +7,18 @@ const TweetReplies = ({ replyTweets }: { replyTweets: Tweet[] }) => {
     <div>
       {replyTweets.map((replyTweet) => {
         return (
-          <div key={replyTweet.id}>
-            <p>{replyTweet.user.username}</p>
-            <p>{replyTweet.body}</p>
+          <div key={replyTweet.id} className="border-b border-white px-4 flex items-center">
+            <img
+              className="w-16 h-16 rounded-full"
+              src={replyTweet.user.avatar_url || "/src/assets/profile.svg"}
+              alt="profile avatar"
+            />
+            <div>
+              <p>{replyTweet.user.display_name}</p>
+              <p>{replyTweet.user.username}</p>
+              <p>{replyTweet.created_at}</p>
+              <p>{replyTweet.body}</p>
+            </div>
           </div>
         );
       })}
