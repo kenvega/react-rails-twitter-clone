07ee8d3458a088a4bc4ae3188a225bbf52136823
Ken Vega <ken.vega.g@gmail.com>
2025-08-29 12:04:46 -0500

    main - wip reply tweets

--- client/src/components/TweetReplies.tsx
+++ client/src/components/TweetReplies.tsx
@@ -7,17 +7,35 @@ const TweetReplies = ({ replyTweets }: { replyTweets: Tweet[] }) => {
     <div>
       {replyTweets.map((replyTweet) => {
         return (
-          <div key={replyTweet.id} className="border-b border-white px-4 flex items-center">
-            <img
-              className="w-16 h-16 rounded-full"
-              src={replyTweet.user.avatar_url || "/src/assets/profile.svg"}
-              alt="profile avatar"
-            />
-            <div>
-              <p>{replyTweet.user.display_name}</p>
-              <p>{replyTweet.user.username}</p>
-              <p>{replyTweet.created_at}</p>
-              <p>{replyTweet.body}</p>
+          <div key={replyTweet.id} className="border-b border-white px-4 ">
+            <div className="flex items-center">
+              <img
+                className="w-16 h-16 rounded-full"
+                src={replyTweet.user.avatar_url || "/src/assets/profile.svg"}
+                alt="profile avatar"
+              />
+              <div className="ml-4">
+                <p>
+                  <span className="font-bold">{replyTweet.user.display_name}</span>{" "}
+                  <span className="text-slate-500">@{replyTweet.user.username}</span> ·{" "}
+                  <span className="text-slate-500">{replyTweet.created_at}</span>
+                </p>
+                <p>{replyTweet.body}</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <button className="flex cursor-pointer">
+                <img
+                  src={
+                    replyTweet.tweet_retweeted_by_current_user
+                      ? "/src/assets/retweet-filled.svg"
+                      : "/src/assets/retweet-unfilled.svg"
+                  }
+                  className={`w-4 mr-2`}
+                  alt="retweet icon"
+                />
+                <span>{replyTweet.retweets_count}</span>
+              </button>
             </div>
           </div>
         );
