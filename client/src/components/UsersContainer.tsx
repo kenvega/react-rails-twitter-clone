b6af1931ec99dcbdeeee47eeff2b1fc469b2a839
Ken Vega <ken.vega.g@gmail.com>
2025-09-06 10:48:04 -0500

    main - add user interface

--- client/src/components/UsersContainer.tsx
+++ client/src/components/UsersContainer.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { getUsers } from "../services/userService";
+import { User } from "../interfaces/User";
 
 const UsersContainer = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [loadingUsers, setLoadingUsers] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -28,14 +29,27 @@ const UsersContainer = () => {
   return (
     <div>
       <div>
-        {users.map((user) => {
-          return (
-            <div key={user.id}>
-              <p>user:</p>
-              <p>email: {user.email}</p>
+        {users.map((user) => (
+          <div key={user.id} className="flex justify-around items-center pt-4 pb-4 border-b border-sky-200">
+            <div className="flex items-center">
+              <a className="mr-2">
+                <img
+                  src={user.avatar_url || "/src/assets/profile.svg"}
+                  alt={`${user.display_name} avatar`}
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+              </a>
+              <div>
+                <div>{user.display_name}</div>
+                <div className="text-gray-500">@{user.username}</div>
+              </div>
             </div>
-          );
-        })}
+
+            <div>
+              <a className="px-4 py-1 border border-sky-500 text-sky-600 rounded-full hover:bg-sky-50">Go to profile</a>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ client/src/interfaces/User.tsx
@@ -0,0 +1,7 @@
+export interface User {
+  id: number;
+  email: string;
+  display_name: string;
+  username: string;
+  avatar_url: string;
+}
