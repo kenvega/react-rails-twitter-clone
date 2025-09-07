27d3f4a1a88802efc5d27e2efad3c50917b27110
Ken Vega <ken.vega.g@gmail.com>
2025-09-07 11:40:33 -0500

    main - update UsersContainer

--- client/src/components/UsersContainer.tsx
+++ client/src/components/UsersContainer.tsx
@@ -26,30 +26,41 @@ const UsersContainer = () => {
     fetchUsers();
   }, []);
 
+  if (error) {
+    return <p>Error ocurred</p>;
+  }
+
   return (
     <div>
+      <div className="text-2xl font-semibold mb-3">All Users</div>
       <div>
-        {users.map((user) => (
-          <div key={user.id} className="flex justify-around items-center pt-4 pb-4 border-b border-sky-200">
-            <div className="flex items-center">
-              <a className="mr-2">
-                <img
-                  src={user.avatar_url || "/src/assets/profile.svg"}
-                  alt={`${user.display_name} avatar`}
-                  className="w-12 h-12 rounded-full object-cover"
-                />
-              </a>
-              <div>
-                <div>{user.display_name}</div>
-                <div className="text-gray-500">@{user.username}</div>
+        {loadingUsers ? (
+          <p>Loading...</p>
+        ) : (
+          users.map((user) => (
+            <div key={user.id} className="flex justify-between items-center pt-4 pb-4 border-b border-sky-200">
+              <div className="flex items-center ml-6">
+                <a className="mr-2">
+                  <img
+                    src={user.avatar_url || "/src/assets/profile.svg"}
+                    alt={`${user.display_name} avatar`}
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                </a>
+                <div>
+                  <div>{user.display_name}</div>
+                  <div className="text-gray-500">@{user.username}</div>
+                </div>
+              </div>
+
+              <div className="mr-6">
+                <a className="px-4 py-1 border border-sky-500 text-sky-600 rounded-full hover:bg-sky-50">
+                  Go to profile
+                </a>
               </div>
             </div>
-
-            <div>
-              <a className="px-4 py-1 border border-sky-500 text-sky-600 rounded-full hover:bg-sky-50">Go to profile</a>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
