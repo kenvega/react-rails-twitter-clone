cdabe8d59418b174f09458ea1a8baff091223b5b
Ken Vega <ken.vega.g@gmail.com>
2025-09-17 01:02:35 -0500

    main - add pagelayout

---
+++ client/src/components/layout/PageLayout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import Sidebar from "../Sidebar";
+import HashtagSidebar from "../HashtagSidebar";
+
+type PageLayoutProps = {
+  children: ReactNode;
+  leftSidebar?: ReactNode | null;
+  rightSidebar?: ReactNode | null;
+  className?: string;
+};
+
+const PageLayout = ({
+  children,
+  leftSidebar = <Sidebar />,
+  rightSidebar = <HashtagSidebar />,
+  className = "",
+}: PageLayoutProps) => (
+  <div className={`py-4 min-h-screen dark:bg-gray-800 ${className}`}>
+    <div className="flex flex-row max-w-7xl mx-auto">
+      <div className="basis-1/5">{leftSidebar}</div>
+      <div className="basis-3/5">{children}</div>
+      <div className="basis-1/5">{rightSidebar}</div>
+    </div>
+  </div>
+);
+
+export default PageLayout;
